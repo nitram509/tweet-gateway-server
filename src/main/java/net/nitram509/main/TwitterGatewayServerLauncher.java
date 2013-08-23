b3296b2da614b58bb5c311429662b262e1e29a85
nitram509 <maki@bitkings.de>
2013-08-23 16:17:46 +0200

    upgraded to Jetty 9 inserted index.html Resource Handler fixed resource compilation error

---
+++ src/main/java/net/nitram509/main/TwitterGatewayServerLauncher.java
@@ -0,0 +1,53 @@
+package net.nitram509.main;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class TwitterGatewayServerLauncher {
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(getPort());
+
+    HandlerList handlerList = new HandlerList();
+    handlerList.addHandler(createServletContextHandler());
+    handlerList.addHandler(createDefaultHandler());
+    server.setHandler(handlerList);
+
+    server.start();
+    server.join();
+  }
+
+  private static ServletContextHandler createServletContextHandler() {
+    ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    servletContextHandler.setContextPath("/");
+    servletContextHandler.addServlet(createRestServletHolder(), "/rest/*");
+    return servletContextHandler;
+  }
+
+  private static ServletHolder createRestServletHolder() {
+    ServletHolder restServletHolder = new ServletHolder();
+    restServletHolder.setName("Jersey REST Service");
+    restServletHolder.setClassName("com.sun.jersey.spi.container.servlet.ServletContainer");
+    restServletHolder.getInitParameters().put("com.sun.jersey.config.property.packages", "net.nitram509");
+    restServletHolder.setInitOrder(1);
+    return restServletHolder;
+  }
+
+  private static Handler createDefaultHandler() {
+    ResourceHandler resourceHandler = new ResourceHandler();
+    resourceHandler.setResourceBase(".");
+    resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+    resourceHandler.setDirectoriesListed(false);
+    return resourceHandler;
+  }
+
+  private static int getPort() {
+    String port = System.getenv("PORT") == null ? "5000" : System.getenv("PORT");
+    return Integer.parseInt(port);
+  }
+}
