a3d7b6d8747c8adb096e9c4d893af2ad3ed03ef0
nitram509 <maki@bitkings.de>
2014-02-05 23:09:53 +0100

    in case it's easier ... there is UriInfo from  jaxws

--- src/main/java/net/nitram509/controller/CallbackHttpController.java
+++ src/main/java/net/nitram509/controller/CallbackHttpController.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,6 +25,9 @@ import static net.nitram509.controller.SignInHttpController.DO_TWITTER_CALLBACK;
 
 @Path(DO_TWITTER_CALLBACK)
 public class CallbackHttpController {
+
+  @Context
+  UriInfo uriInfo;
 
   @GET
   @Produces({TEXT_PLAIN})
