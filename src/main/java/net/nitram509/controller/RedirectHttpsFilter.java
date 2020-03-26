c05ebbd84e8fa3e88d64edf5c147688dd60285ed
nitram509 <maki@bitkings.de>
2020-03-26 21:28:27 +0100

    optimize imports

--- src/main/java/net/nitram509/controller/RedirectHttpsFilter.java
+++ src/main/java/net/nitram509/controller/RedirectHttpsFilter.java
@@ -2,11 +2,6 @@ package net.nitram509.controller;
 
 import org.glassfish.jersey.server.ContainerRequest;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -26,7 +21,6 @@ public class RedirectHttpsFilter implements ContainerRequestFilter {
   @Override
   public void filter(ContainerRequestContext ctx) throws IOException {
     String x_forwarded_proto_header = ctx.getHeaderString(X_FORWARDED_PROTO);
-    System.out.println(x_forwarded_proto_header);
     if (x_forwarded_proto_header != null && !x_forwarded_proto_header.startsWith("https")) {
       ContainerRequest request = (ContainerRequest) ctx.getRequest();
       String path = (request.getPath(false) != null) ? request.getPath(false) : "";
